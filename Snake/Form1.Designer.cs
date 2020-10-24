5920783a57e9ec595c0fd4ecda4f773f8fda9612
mihai calitescu <mihai_gabriel18@yahoo.com>
2020-10-24 17:27:51 +0300

    Added pictures and retry feature

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -29,7 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Snake));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
@@ -38,6 +40,9 @@
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -45,7 +50,7 @@
             this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
             this.tableLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.tableLayoutPanel1.ColumnCount = SnakeInstance.GridSize;
+            this.tableLayoutPanel1.ColumnCount = 30;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
@@ -86,9 +91,9 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, SnakeInstance.GridSize);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(22, 29);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = SnakeInstance.GridSize;
+            this.tableLayoutPanel1.RowCount = 30;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
@@ -133,6 +138,16 @@
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.TableLayoutPanel1_CellPaint);
             // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Location = new System.Drawing.Point(722, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 200);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "EASY";
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
@@ -140,13 +155,15 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.richTextBox1.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox1.Location = new System.Drawing.Point(681, 209);
+            this.richTextBox1.Location = new System.Drawing.Point(712, 185);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(187, 65);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "SCORE";
+            this.richTextBox1.Visible = false;
             // 
             // contextMenuStrip2
             // 
@@ -155,58 +172,112 @@
             // 
             // richTextBox2
             // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.richTextBox2.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(712, 280);
+            this.richTextBox2.Location = new System.Drawing.Point(737, 256);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(134, 76);
+            this.richTextBox2.Size = new System.Drawing.Size(136, 61);
             this.richTextBox2.TabIndex = 4;
             this.richTextBox2.Text = "0";
+            this.richTextBox2.Visible = false;
             // 
             // richTextBox3
             // 
+            this.richTextBox3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.richTextBox3.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox3.Location = new System.Drawing.Point(681, 362);
+            this.richTextBox3.Location = new System.Drawing.Point(712, 323);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.ReadOnly = true;
             this.richTextBox3.Size = new System.Drawing.Size(187, 65);
             this.richTextBox3.TabIndex = 5;
             this.richTextBox3.Text = "POINTS";
+            this.richTextBox3.Visible = false;
             // 
             // richTextBox4
             // 
+            this.richTextBox4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.richTextBox4.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.richTextBox4.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox4.Location = new System.Drawing.Point(681, 44);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.ShowSelectionMargin = true;
             this.richTextBox4.Size = new System.Drawing.Size(250, 122);
             this.richTextBox4.TabIndex = 7;
             this.richTextBox4.Text = "PRESS ANY OF THE ARROW KEYS TO START";
+            this.richTextBox4.Visible = false;
             // 
             // richTextBox5
             // 
+            this.richTextBox5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox5.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox5.Location = new System.Drawing.Point(681, 44);
             this.richTextBox5.Name = "richTextBox5";
-            this.richTextBox5.Size = new System.Drawing.Size(250, 122);
+            this.richTextBox5.Size = new System.Drawing.Size(250, 47);
             this.richTextBox5.TabIndex = 9;
             this.richTextBox5.Text = "GAME OVER";
             this.richTextBox5.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(681, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 206);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "PLAY AGAIN";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.Location = new System.Drawing.Point(722, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 200);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "HARD";
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.Location = new System.Drawing.Point(722, 218);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 200);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "MEDIUM";
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
             // 
             // Snake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1017, 655);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(997, 655);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox5);
             this.Controls.Add(this.richTextBox4);
             this.Controls.Add(this.richTextBox3);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "Snake";
-            this.Text = "Form1";
+            this.Text = "SNAKE";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
@@ -225,6 +296,10 @@
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.RichTextBox richTextBox4;
         private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,140 +12,10 @@ using System.Windows.Forms;
 namespace Snake
 {
 
-    public class SnakeInstance
-    {
-        // The size of the game board
-        private static int GRID_SIZE;
-        // The map of the whole game,
-        private Color[,] mapColor;
-        // Speed of the snake and basically the difficulty of the game
-        private int SNAKE_SPEED;
-        // 0 for up // 1 for right // 2 for down // 3 for left
-        private int direction;
-        // Initial direction of the snake, same codification as direction
-        private int INITIAL_DIRECTION;
-        // The item that was just enqueued (a peek that works on the other side)
-        private Tuple<int, int> justEnqueuedItem;
-        // Queue with all of the snake's blocks
-        private Queue<Tuple<int, int>> position;
-
-        // default constructor
-        public SnakeInstance()
-        {
-            position = new Queue<Tuple<int, int>>();
-        }
-
-        public SnakeInstance(string difficulty, string INITIAL_DIRECTION)
-        {
-            switch (difficulty)
-            {
-                case "EASY":
-                    SNAKE_SPEED = 100;
-                    break;
-                case "MEDIUM":
-                    SNAKE_SPEED = 200;
-                    break;
-                case "HARD":
-                    SNAKE_SPEED = 400;
-                    break;
-                default:
-                    throw new ArgumentException("Invalid difficulty: ", difficulty);
-            }
-
-            switch (INITIAL_DIRECTION)
-            {
-                case "LEFT":
-                    this.INITIAL_DIRECTION = 3;
-                    break;
-                case "UP":
-                    this.INITIAL_DIRECTION = 0;
-                    break;
-                case "RIGHT":
-                    this.INITIAL_DIRECTION = 1;
-                    break;
-                case "DOWN":
-                    this.INITIAL_DIRECTION = 2;
-                    break;
-                default:
-                    throw new ArgumentException("Invalid direction: ", INITIAL_DIRECTION);
-            }
-
-            GRID_SIZE = 30;
-            this.position = new Queue<Tuple<int, int>>();
-            this.mapColor = new Color[GRID_SIZE, GRID_SIZE];
-            this.initilizeSnake(mapColor);
-
-            SnakeSpeed = SNAKE_SPEED;
-            Direction = direction;
-            JustEnqueuedItem = justEnqueuedItem;
-            Position = position;
-            MapColor = mapColor;
-            GridSize = GRID_SIZE;
-        }
-
-        // getters and setters
-
-        public static int GridSize { get; set; }
-
-        public int SnakeSpeed { get; set; }
-
-        public int Direction { get; set; }
-
-        public Tuple<int, int> JustEnqueuedItem { get; set; }
-
-        public Queue<Tuple<int, int>> Position { get; set; }
-
-        public Color[,] MapColor { get; set; }
-
-
-        private void initilizeSnake(Color[,] color)
-        {
-
-            // we put the snake in a random position !!!!!!!!
-            Tuple<int, int> point1 = new Tuple<int, int>(2, 14);
-            direction = INITIAL_DIRECTION;
-
-            position.Enqueue(point1);
-            color[point1.Item1, point1.Item2] = Color.Green;
-
-            Tuple<int, int> point2 = new Tuple<int, int>(point1.Item1 + 1, point1.Item2);
-            position.Enqueue(point2);
-            color[point2.Item1, point2.Item2] = Color.Green;
-
-            Tuple<int, int> point3 = new Tuple<int, int>(point2.Item1 + 1, point2.Item2);
-            position.Enqueue(point3);
-            color[point3.Item1, point3.Item2] = Color.Green;
-
-            Tuple<int, int> point4 = new Tuple<int, int>(point3.Item1 + 1, point3.Item2);
-            position.Enqueue(point4);
-            color[point4.Item1, point4.Item2] = Color.Green;
-            justEnqueuedItem = point4;
-
-            this.generateFood();
-        }
-
-        public void generateFood()
-        {
-            Random randomX = new Random();
-            int randomNumberX = randomX.Next(0, GRID_SIZE);
-            Random randomY = new Random();
-            int randomNumberY = randomY.Next(0, GRID_SIZE);
-
-            while (this.mapColor[randomNumberX, randomNumberY] == Color.Green)
-            {
-                randomX = new Random();
-                randomNumberX = randomX.Next(0, GRID_SIZE);
-                randomY = new Random();
-                randomNumberY = randomY.Next(0, GRID_SIZE);
-            }
-
-            this.mapColor[randomNumberX, randomNumberY] = Color.Red;
-        }
-
-    }
-
     public partial class Snake : Form
     {
+        // Checking if the difficulty has been selected
+        private bool difficultySelected;
         // The score of the current game
         private static int score;
         // We check if we are at the first buttom press, the one that start the game
@@ -156,7 +26,8 @@ namespace Snake
         // Default constructor
         public Snake()
         {
-            snake = new SnakeInstance("EASY", "RIGHT");
+            difficultySelected = false;
+            snake = new SnakeInstance("RIGHT");
 
             score = 0;
             startGame = true;
@@ -164,7 +35,6 @@ namespace Snake
             // initializing the starting position of the snake
             InitializeComponent();
 
-            timer1.Interval = snake.SnakeSpeed;
             timer1.Tick += Timer1_Tick;
         }
 
@@ -233,19 +103,22 @@ namespace Snake
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (startGame)
+            if (difficultySelected)
             {
-                if (e.KeyCode.Equals(Keys.Down) || e.KeyCode.Equals(Keys.Up) ||
-                    e.KeyCode.Equals(Keys.Left) || e.KeyCode.Equals(Keys.Right))
+                if (startGame)
                 {
-                    timer1.Start();
-                    startGame = false;
+                    if (e.KeyCode.Equals(Keys.Down) || e.KeyCode.Equals(Keys.Up) ||
+                        e.KeyCode.Equals(Keys.Left) || e.KeyCode.Equals(Keys.Right))
+                    {
+                        timer1.Start();
+                        startGame = false;
+                    }
                 }
-            }
-            else
-            {
-                ChangeState(e.KeyCode, true);
-                e.SuppressKeyPress = true;
+                else
+                {
+                    ChangeState(e.KeyCode, true);
+                    e.SuppressKeyPress = true;
+                }
             }
         }
 
@@ -291,6 +164,7 @@ namespace Snake
             {
                 richTextBox5.Visible = true;
                 richTextBox4.Visible = false;
+                button1.Visible = true;
                 timer1.Stop();
                 return;
             }
@@ -319,6 +193,189 @@ namespace Snake
             keyTable.Clear();
 
             tableLayoutPanel1.Refresh();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SnakeInstance.GridSize; i++)
+                for (int j = 0; j < SnakeInstance.GridSize; j++)
+                    snake.MapColor[i, j] = Color.White;
+
+            startGame = true;
+            difficultySelected = false;
+
+            button2.Visible = true;
+            button4.Visible = true;
+            button5.Visible = true;
+            richTextBox1.Visible = false;
+            richTextBox2.Visible = false;
+            richTextBox3.Visible = false;
+            richTextBox4.Visible = false;
+            difficultySelected = false;
+            richTextBox5.Visible = false;
+            button1.Visible = false;
+
+            snake = new SnakeInstance("RIGHT");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            snake.SnakeSpeed = 200;
+            timer1.Interval = snake.SnakeSpeed;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = true;
+            richTextBox3.Visible = true;
+            richTextBox4.Visible = true;
+            difficultySelected = true;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            snake.SnakeSpeed = 100;
+            timer1.Interval = snake.SnakeSpeed;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = true;
+            richTextBox3.Visible = true;
+            richTextBox4.Visible = true;
+            difficultySelected = true;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+            snake.SnakeSpeed = 50;
+            timer1.Interval = snake.SnakeSpeed;
+            button2.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = true;
+            richTextBox3.Visible = true;
+            richTextBox4.Visible = true;
+            difficultySelected = true;
+        }
+    }
+
+    public class SnakeInstance
+    {
+        // The size of the game board
+        private static int GRID_SIZE = 30;
+        // The map of the whole game,
+        private Color[,] mapColor;
+        // Speed of the snake and basically the difficulty of the game
+        private int SNAKE_SPEED;
+        // 0 for up // 1 for right // 2 for down // 3 for left
+        private int direction;
+        // Initial direction of the snake, same codification as direction
+        private int INITIAL_DIRECTION;
+        // The item that was just enqueued (a peek that works on the other side)
+        private Tuple<int, int> justEnqueuedItem;
+        // Queue with all of the snake's blocks
+        private Queue<Tuple<int, int>> position;
+
+        // default constructor
+        public SnakeInstance()
+        {
+            position = new Queue<Tuple<int, int>>();
+        }
+
+        public SnakeInstance(string INITIAL_DIRECTION)
+        {
+            switch (INITIAL_DIRECTION)
+            {
+                case "LEFT":
+                    this.INITIAL_DIRECTION = 3;
+                    break;
+                case "UP":
+                    this.INITIAL_DIRECTION = 0;
+                    break;
+                case "RIGHT":
+                    this.INITIAL_DIRECTION = 1;
+                    break;
+                case "DOWN":
+                    this.INITIAL_DIRECTION = 2;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid direction: ", INITIAL_DIRECTION);
+            }
+
+            GRID_SIZE = 30;
+            position = new Queue<Tuple<int, int>>();
+            mapColor = new Color[GRID_SIZE, GRID_SIZE];
+            initilizeSnake(mapColor);
+
+            SnakeSpeed = SNAKE_SPEED;
+            Direction = direction;
+            JustEnqueuedItem = justEnqueuedItem;
+            Position = position;
+            MapColor = mapColor;
+            GridSize = GRID_SIZE;
+        }
+
+        // getters and setters
+
+        public static int GridSize { get; set; }
+
+        public int SnakeSpeed { get; set; }
+
+        public int Direction { get; set; }
+
+        public Tuple<int, int> JustEnqueuedItem { get; set; }
+
+        public Queue<Tuple<int, int>> Position { get; set; }
+
+        public Color[,] MapColor { get; set; }
+
+        private void initilizeSnake(Color[,] color)
+        {
+
+            // we put the snake in a random position !!!!!!!!
+            Tuple<int, int> point1 = new Tuple<int, int>(2, 14);
+            direction = INITIAL_DIRECTION;
+
+            position.Enqueue(point1);
+            color[point1.Item1, point1.Item2] = Color.Green;
+
+            Tuple<int, int> point2 = new Tuple<int, int>(point1.Item1 + 1, point1.Item2);
+            position.Enqueue(point2);
+            color[point2.Item1, point2.Item2] = Color.Green;
+
+            Tuple<int, int> point3 = new Tuple<int, int>(point2.Item1 + 1, point2.Item2);
+            position.Enqueue(point3);
+            color[point3.Item1, point3.Item2] = Color.Green;
+
+            Tuple<int, int> point4 = new Tuple<int, int>(point3.Item1 + 1, point3.Item2);
+            position.Enqueue(point4);
+            color[point4.Item1, point4.Item2] = Color.Green;
+            justEnqueuedItem = point4;
+
+            generateFood();
+        }
+
+
+
+        public void generateFood()
+        {
+            Random randomX = new Random();
+            int randomNumberX = randomX.Next(0, GRID_SIZE);
+            Random randomY = new Random();
+            int randomNumberY = randomY.Next(0, GRID_SIZE);
+
+            while (mapColor[randomNumberX, randomNumberY] == Color.Green)
+            {
+                randomX = new Random();
+                randomNumberX = randomX.Next(0, GRID_SIZE);
+                randomY = new Random();
+                randomNumberY = randomY.Next(0, GRID_SIZE);
+            }
+
+            mapColor[randomNumberX, randomNumberY] = Color.Red;
         }
 
     }
